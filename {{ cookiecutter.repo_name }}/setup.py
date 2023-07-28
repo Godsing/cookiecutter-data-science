ed643a4b6167ab86898bc1464348a68831d3eb6d
Godsing <zzzpwang@gmail.com>
2023-07-28 18:22:40 +0800

    修改项目结构， 使项目便于打包发布

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,10 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
-    packages=find_packages(),
+    name='{{ cookiecutter.package_name }}',
     version='0.0.1',
+    package_dir={"": "src"},
+    packages=find_packages("src"),
     description='{{ cookiecutter.description }}',
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    keywords=[],
     author='{{ cookiecutter.author_name }}',
     license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
 )
